891aea7d6c7bcaf6840565d96ccf6f59fe85407c
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2025-01-10 14:16:09 +0900

    feat/#7 : core모듈 공통 빌드 및 의존성 설정

--- build-logic/src/main/java/com/yapp/app/KotlinAndroid.kt
+++ build-logic/src/main/java/com/yapp/app/KotlinAndroid.kt
@@ -2,6 +2,7 @@ package com.yapp.app
 
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -10,6 +11,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 /**
  * NIA 와 드로디드 나이츠 앱 참고
  * https://github.com/android/nowinandroid/blob/main/build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
+ *
+ * 안드로이드 모듈 설정
  */
 internal fun Project.configureKotlinAndroid() {
     // Plugins
@@ -40,20 +43,37 @@ internal fun Project.configureKotlinAndroid() {
         }
 
     }
-    configureKotlin()
+    configureKotlinCommon(jvmTarget = JvmTarget.JVM_17)
 }
 
-private fun Project.configureKotlin() {
+
+/**
+ * 라이브러리 모듈 설정
+ */
+internal fun Project.configureKotlin() {
+    // Apply common Kotlin settings
+    configureKotlinCommon(jvmTarget = JvmTarget.JVM_17)
+
+    // Additional settings for JVM projects
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
+}
+
+
+/**
+ * Kotlin 프로젝트 공통 설정
+ */
+private fun Project.configureKotlinCommon(jvmTarget: JvmTarget) {
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-            // Treat all Kotlin warnings as errors (disabled by default)
-            // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
+            this.jvmTarget.set(jvmTarget)
             val warningsAsErrors: String? by project
             allWarningsAsErrors.set(warningsAsErrors.toBoolean())
             freeCompilerArgs.set(
                 freeCompilerArgs.get() + listOf(
-                    "-opt-in=kotlin.RequiresOptIn",
+                    "-opt-in=kotlin.RequiresOptIn"
                 )
             )
         }
---
+++ build-logic/src/main/java/yapp.kotlin.library.gradle.kts
@@ -0,0 +1,7 @@
+import com.yapp.app.configureKotlin
+
+plugins {
+    kotlin("jvm")
+}
+
+configureKotlin()
