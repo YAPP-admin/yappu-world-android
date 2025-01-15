6e6f2e9e2b6e212f6982d8dbde36a17d756dd872
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2025-01-15 21:42:54 +0900

    feat/#10 : 어노테이션 생성 ( home 모듈에 EntryPoint )

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,6 @@ import com.yapp.app.setNamespace
 
 plugins {
     id("yapp.android.application")
-
-    /** Hilt 적용하며 compose 및 android 플러그인 제거 예정 **/
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -18,7 +14,4 @@ dependencies {
     implementation(project(":feature:signup"))
     implementation(project(":feature:login"))
 
-
-    /** Hilt 적용하며 compose 및 android 라이브러리 제거 예정 **/
-    implementation(libs.androidx.core.ktx)
 }
