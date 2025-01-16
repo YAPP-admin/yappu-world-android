8e88684445bf97854e5876c5ee742105a1a08293
asheon12 <asheon12@naver.com>
2025-01-17 01:55:56 +0900

    feat/#10 : designssytem 호출

--- feature/home/src/main/java/com/yapp/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/yapp/feature/home/HomeScreen.kt
@@ -1,9 +1,14 @@
 package com.yapp.feature.home
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.yapp.core.designsystem.component.button.text.YappTextAssistiveButtonSmall
 import com.yapp.core.designsystem.theme.YappTheme
 
 
@@ -11,8 +16,14 @@ import com.yapp.core.designsystem.theme.YappTheme
 fun HomeScreen (
 ){
     YappTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+        Column(
+            modifier = Modifier.windowInsetsPadding(WindowInsets.safeContent),
+            verticalArrangement = Arrangement.spacedBy(12.dp)) {
+            YappTextAssistiveButtonSmall(
+                text = "Home",
+                enable = true,
+                onClick = {}
+            )
         }
     }
 }
