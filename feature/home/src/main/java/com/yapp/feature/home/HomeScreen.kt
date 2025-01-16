71852415aa119ec096bcd972ce0019f01db49071
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2025-01-17 00:00:37 +0900

    feat/#10 : MainActivity 이동

---
+++ feature/home/src/main/java/com/yapp/feature/home/HomeScreen.kt
@@ -0,0 +1,19 @@
+package com.yapp.feature.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.yapp.core.designsystem.theme.YappTheme
+
+
+@Composable
+fun HomeScreen (
+){
+    YappTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        }
+    }
+}
+
