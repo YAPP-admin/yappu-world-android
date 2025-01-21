9f9ccf95857d5b05de7fc7725247f4e517c35559
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-01-21 17:37:51 +0900

    feat/#26 : navigator 상태 추가

---
+++ app/src/main/java/com/yapp/app/official/ui/Navigator.kt
@@ -0,0 +1,54 @@
+package com.yapp.app.official.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.yapp.feature.home.navigation.navigateToHome
+import com.yapp.feature.login.navigation.LoginRoute
+import com.yapp.feature.login.navigation.navigateToLogin
+import com.yapp.feature.notice.navigation.navigateToNotice
+import com.yapp.feature.signup.navigation.navigateToSignUp
+
+
+@Composable
+fun rememberNavigator(
+    navController: NavHostController = rememberNavController(),
+): NavigatorState = remember(navController) {
+    NavigatorState(
+        navController = navController,
+    )
+}
+
+
+class NavigatorState(
+    val navController: NavHostController,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val startDestination = LoginRoute
+
+    fun navigateLoginScreen() {
+        navController.navigateToLogin()
+    }
+
+    fun navigateSignUpScreen() {
+        navController.navigateToSignUp()
+    }
+
+    fun navigateHomeScreen() {
+        navController.navigateToHome()
+    }
+
+    fun navigateNoticeScreen() {
+        navController.navigateToNotice()
+    }
+
+    private fun popBackStack() {
+        navController.popBackStack()
+    }
+}
