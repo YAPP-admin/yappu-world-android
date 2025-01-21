67b7dd7e6b87108623c1e84384520098bac8bd93
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-01-21 17:41:26 +0900

    feat/#26 : navHost 생성

---
+++ app/src/main/java/com/yapp/app/official/navigation/YappNavHost.kt
@@ -0,0 +1,30 @@
+package com.yapp.app.official.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.yapp.app.official.ui.NavigatorState
+import com.yapp.feature.home.navigation.homeNavGraph
+import com.yapp.feature.login.navigation.loginNavGraph
+import com.yapp.feature.notice.navigation.noticeNavGraph
+import com.yapp.feature.signup.navigation.signupNavGraph
+
+
+@Composable
+fun YappNavHost(
+    navigator: NavigatorState,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navigator.navController,
+        startDestination = navigator.startDestination,
+        modifier = modifier,
+    ) {
+        loginNavGraph(
+            onSignUpClick = { navigator.navigateSignUpScreen() }
+        )
+        signupNavGraph()
+        homeNavGraph()
+        noticeNavGraph()
+    }
+}
