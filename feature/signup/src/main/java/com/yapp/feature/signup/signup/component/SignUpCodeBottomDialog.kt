acd4acc1c36a9cbc248ea1150e3f6b2dc5690555
jinukeu <koownij@kakao.com>
2025-01-27 11:06:37 +0900

    feat/#40: SignUpScreen Step4 View

---
+++ feature/signup/src/main/java/com/yapp/feature/signup/signup/component/SignUpCodeBottomDialog.kt
@@ -0,0 +1,60 @@
+package com.yapp.feature.signup.signup.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.yapp.core.designsystem.component.button.solid.YappSolidPrimaryButtonLarge
+import com.yapp.core.designsystem.component.button.text.YappTextPrimaryButtonSmall
+import com.yapp.core.designsystem.component.input.text.YappInputTextLarge
+import com.yapp.core.designsystem.theme.YappTheme
+import com.yapp.core.ui.component.BottomDialog
+import com.yapp.feature.signup.R
+
+@Composable
+fun SignUpCodeBottomDialog() {
+    BottomDialog(
+        onDismiss = {}
+    ) {
+        Column(
+            modifier = Modifier.padding(20.dp),
+        ) {
+            Text(
+                text = stringResource(R.string.signup_code_bottom_dialog_title),
+                style = YappTheme.typography.headline1Bold,
+                color = YappTheme.colorScheme.labelNormal
+            )
+
+            Spacer(Modifier.height(24.dp))
+
+            YappInputTextLarge(
+                label = stringResource(R.string.signup_code_bottom_dialog_input_text_label),
+                value = "",
+                placeholder = stringResource(R.string.signup_code_bottom_dialog_input_text_placeholder),
+                onValueChange = {}
+            )
+
+            Spacer(Modifier.height(24.dp))
+
+            YappSolidPrimaryButtonLarge(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(R.string.signup_code_bottom_dialog_complete_button),
+                onClick = {}
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            YappTextPrimaryButtonSmall(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(R.string.signup_code_bottom_dialog_no_code_button),
+                onClick = {}
+            )
+        }
+    }
+}
