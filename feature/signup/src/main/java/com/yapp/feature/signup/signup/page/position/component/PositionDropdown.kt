95bfd2c483c277b030046682cd3507a7857f848a
jinukeu <koownij@kakao.com>
2025-01-28 23:51:55 +0900

    feat/#48: ActivityUnitInputSection 별도의 파일로 분리

--- feature/signup/src/main/java/com/yapp/feature/signup/signup/page/position/component/PositionDropdown.kt
+++ feature/signup/src/main/java/com/yapp/feature/signup/signup/page/position/component/PositionDropdown.kt
@@ -1,15 +1,9 @@
-package com.yapp.feature.signup.signup.component
+package com.yapp.feature.signup.signup.page.position.component
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,7 +26,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
 import com.yapp.core.designsystem.component.input.text.InputTextDefaults
 import com.yapp.core.designsystem.component.input.text.YappBasicInputText
 import com.yapp.core.designsystem.extension.yappClickable
