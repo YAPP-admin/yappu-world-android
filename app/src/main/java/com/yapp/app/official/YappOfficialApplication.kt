2200cb7b1c74d0635c6398a90a8a14e2f5b73295
jinukeu <koownij@kakao.com>
2025-01-28 23:46:42 +0900

    feat/#51: Timber 세팅

--- app/src/main/java/com/yapp/app/official/YappOfficialApplication.kt
+++ app/src/main/java/com/yapp/app/official/YappOfficialApplication.kt
@@ -2,7 +2,16 @@ package com.yapp.app.official
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 
 @HiltAndroidApp
-class YappOfficialApplication : Application()
+class YappOfficialApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
