25bef846141fe4f0bad844e751ca1399d5c3aef8
jinukeu <koownij@kakao.com>
2025-01-28 17:38:35 +0900

    feat/#48: BottomDialog imePadding 관련 bouncing 이슈가 있어 Dialog -> BottomSheet로 변경

--- core/ui/src/main/java/com/yapp/core/ui/component/BottomDialog.kt
+++ core/ui/src/main/java/com/yapp/core/ui/component/BottomDialog.kt
@@ -1,50 +1,46 @@
 package com.yapp.core.ui.component
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import com.yapp.core.designsystem.theme.YappTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomDialog(
     onDismiss: () -> Unit,
     content: @Composable () -> Unit,
 ) {
-    Dialog(
+    ModalBottomSheet(
+        modifier = Modifier
+            .imePadding()
+            .padding(8.dp),
+        containerColor = Color.Transparent,
+        contentColor = Color.Transparent,
+        dragHandle = null,
         onDismissRequest = { onDismiss() },
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false,
-            decorFitsSystemWindows = false,
-        )
+        shape = RoundedCornerShape(0.dp),
+        sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     ) {
-        Box(
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = YappTheme.colorScheme.backgroundElevatedNormal,
             modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-                .imePadding()
+                .fillMaxWidth()
         ) {
-            Surface(
-                shape = RoundedCornerShape(16.dp),
-                color = YappTheme.colorScheme.backgroundElevatedNormal,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.BottomCenter)
-            ) {
-                content()
-            }
+            content()
         }
     }
 }
