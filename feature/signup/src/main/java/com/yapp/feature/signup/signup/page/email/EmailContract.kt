f16c8d06173d9b67ed9ce34c91e9406859659209
jinukeu <koownij@kakao.com>
2025-01-30 19:58:52 +0900

    feat/#60: 회원가입 - 이메일 형식 에러시 화면 추가

--- feature/signup/src/main/java/com/yapp/feature/signup/signup/page/email/EmailContract.kt
+++ feature/signup/src/main/java/com/yapp/feature/signup/signup/page/email/EmailContract.kt
@@ -1,8 +1,20 @@
 package com.yapp.feature.signup.signup.page.email
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.yapp.feature.signup.R
+import com.yapp.model.Regex
+
 data class EmailState(
     val email: String = "",
-)
+) {
+    val isEmailError =
+        email.matches(Regex.email).not() && email.isNotEmpty()
+
+    val emailErrorDescription: String?
+        @Composable
+        get() = if (isEmailError) stringResource(R.string.signup_screen_email_input_text_description) else null
+}
 
 sealed interface EmailIntent {
     data class ChangeEmail(val email: String) : EmailIntent
