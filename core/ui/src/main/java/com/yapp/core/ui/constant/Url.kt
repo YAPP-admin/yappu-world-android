f06ec82859271662a2983af23fccac469780af18
jinukeu <koownij@kakao.com>
2025-01-30 14:27:56 +0900

    feat/#66: 약관 및 개인정보처리방침 링크 연결 기능 추가

---
+++ core/ui/src/main/java/com/yapp/core/ui/constant/Url.kt
@@ -0,0 +1,6 @@
+package com.yapp.core.ui.constant
+
+object Url {
+    const val TERMS = "https://yapp-workspace.notion.site/48f4eb2ffdd94740979e8a3b37ca260d"
+    const val PRIVACY_POLICY = "https://yapp-workspace.notion.site/fc24f8ba29c34f9eb30eb945c621c1ca"
+}
