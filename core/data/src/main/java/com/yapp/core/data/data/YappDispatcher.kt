806f01784fbc1244e76f1ee26c2686411f5e41aa
jinukeu <koownij@kakao.com>
2025-02-02 10:14:49 +0900

    refactor/#60: 네이밍 일관성 개선

--- core/data/src/main/java/com/yapp/core/data/data/YappDispatcher.kt
+++ core/data/src/main/java/com/yapp/core/data/data/YappDispatcher.kt
@@ -3,7 +3,7 @@ package com.yapp.core.data.data
 import javax.inject.Qualifier
 
 @Qualifier
-annotation class Dispatcher(val susuDispatcher: YappDispatchers)
+annotation class Dispatcher(val yappDispatcher: YappDispatchers)
 
 enum class YappDispatchers {
     IO,
