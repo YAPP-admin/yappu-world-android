93e03e0be70aab46ee1a33bc8b9a0654a6ef4d11
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-02-07 00:57:40 +0900

    feat/#69 : 홈 화면 구성

--- feature/home/src/main/java/com/yapp/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/yapp/feature/home/HomeScreen.kt
@@ -1,22 +1,86 @@
 package com.yapp.feature.home
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.yapp.core.designsystem.component.button.text.YappTextAssistiveButtonSmall
-
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.yapp.core.designsystem.component.header.YappDefaultHeader
+import com.yapp.core.designsystem.theme.YappTheme
+import com.yapp.core.ui.component.YappBackground
+import com.yapp.core.ui.extension.collectWithLifecycle
+import com.yapp.feature.home.component.NoticeSection
+import com.yapp.feature.home.component.ProfileSection
 
 @Composable
-fun HomeScreen (
-){
-    Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp)) {
-        YappTextAssistiveButtonSmall(
-            text = "Home",
-            enable = true,
-            onClick = {}
-        )
+internal fun HomeRoute(
+    navigateToNotice: () -> Unit,
+    navigateToSetting: () -> Unit,
+    viewModel: HomeViewModel = hiltViewModel(),
+) {
+    val homeState by viewModel.store.uiState.collectAsStateWithLifecycle()
+    viewModel.store.sideEffects.collectWithLifecycle { effect ->
+        when (effect) {
+            HomeSideEffect.NavigateToNotice -> navigateToNotice()
+            HomeSideEffect.NavigateToSetting -> navigateToSetting()
+        }
+    }
+
+    HomeScreen(
+        homeState = homeState,
+        onIntent = { viewModel.store.onIntent(it) }
+    )
+}
+
+@Composable
+fun HomeScreen(
+    homeState: HomeState,
+    onIntent: (HomeIntent) -> Unit = {},
+) {
+    YappBackground(
+        color = YappTheme.colorScheme.backgroundNormalAlternative
+    ) {
+        val scrollState = rememberScrollState()
+        Column {
+            YappDefaultHeader(
+                onClickRightIcon = { onIntent(HomeIntent.ClickSettingButton) }
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .padding(horizontal = 20.dp, vertical = 16.dp)
+            ) {
+                ProfileSection(
+                    homeState.name,
+                    "활동회원",
+                    (homeState.activityUnits[0].generation) ?: 25,
+                    (homeState.activityUnits[0].position) ?: "Android"
+                )
+                Spacer(Modifier.height(8.dp))
+                NoticeSection(
+                    noticeInfo = homeState.noticeInfo,
+                    onMoreButtonClick = { onIntent(HomeIntent.ClickMoreButton)}
+                )
+            }
+        }
     }
 }
 
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    YappTheme {
+        HomeRoute({}, {})
+    }
+}
---
+++ feature/home/src/main/java/com/yapp/feature/home/component/ProfileSection.kt
@@ -0,0 +1,59 @@
+package com.yapp.feature.home.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.yapp.core.designsystem.component.chip.ChipColorType
+import com.yapp.core.designsystem.component.chip.YappChipSmall
+import com.yapp.core.designsystem.theme.YappTheme
+
+@Composable
+fun ProfileSection(
+    name : String,
+    activityStatus : String,
+    generation : Int,
+    position : String
+){
+    HomeSectionBackground {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = name,
+                color = YappTheme.colorScheme.labelNormal,
+                style = YappTheme.typography.title2Bold
+            )
+            Spacer(Modifier.width(8.dp))
+            YappChipSmall(text = activityStatus, colorType = ChipColorType.Main, isFill = false)
+        }
+        Spacer(Modifier.height(4.dp))
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "${generation}기",
+                color = YappTheme.colorScheme.labelAssistive,
+                style = YappTheme.typography.label1NormalMedium
+            )
+            Text(
+                text = "∙",
+                color = YappTheme.colorScheme.labelAssistive,
+                style = YappTheme.typography.label1NormalMedium
+            )
+            Text(
+                text = position,
+                color = YappTheme.colorScheme.labelAssistive,
+                style = YappTheme.typography.label1NormalMedium
+            )
+        }
+    }
+
+}
