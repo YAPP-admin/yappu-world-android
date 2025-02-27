8a0908e679961a455d1ea62b8119f638146e8302
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-02-27 13:42:03 +0900

    feat/#79 : viewmodel & screen 데이터 연동

--- feature/home/src/main/java/com/yapp/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/yapp/feature/home/HomeViewModel.kt
@@ -1,13 +1,21 @@
 package com.yapp.feature.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yapp.core.ui.component.UserRole
 import com.yapp.core.ui.mvi.MviIntentStore
 import com.yapp.core.ui.mvi.mviIntentStore
+import com.yapp.domain.GetUserProfileUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getUserProfileUseCase: GetUserProfileUseCase,
+) : ViewModel() {
 
     val store: MviIntentStore<HomeState, HomeIntent, HomeSideEffect> =
         mviIntentStore(
@@ -15,15 +23,46 @@ class HomeViewModel @Inject constructor() : ViewModel() {
             onIntent = ::onIntent
         )
 
+    init {
+        store.onIntent(HomeIntent.LoadUserInfo)
+    }
+
     private fun onIntent(
         intent: HomeIntent,
         state: HomeState,
         reduce: (HomeState.() -> HomeState) -> Unit,
-        postSideEffect: (HomeSideEffect) -> Unit
-    ){
-        when(intent){
+        postSideEffect: (HomeSideEffect) -> Unit,
+    ) {
+        Log.d("HomeViewModel", "intent :: $intent")
+
+        when (intent) {
             HomeIntent.ClickMoreButton -> postSideEffect(HomeSideEffect.NavigateToNotice)
             HomeIntent.ClickSettingButton -> postSideEffect(HomeSideEffect.NavigateToSetting)
+            HomeIntent.LoadUserInfo -> {
+                loadUserInfo(reduce, postSideEffect)
+            }
         }
+    }
+
+    private fun loadUserInfo(
+        reduce: (HomeState.() -> HomeState) -> Unit,
+        postSideEffect: (HomeSideEffect) -> Unit,
+    ) = viewModelScope.launch {
+        getUserProfileUseCase()
+            .onSuccess { result ->
+                result.firstOrNull()?.let { info ->
+                    reduce {
+                        copy(
+                            name = info.name,
+                            role = UserRole.fromRole(info.role),
+                            activityUnits = info.activityUnits
+                        )
+                    }
+                }
+            }
+            .onFailure {
+                val errorMessage = it.message ?: ""
+                postSideEffect(HomeSideEffect.ShowToast(errorMessage))
+            }
     }
 }
