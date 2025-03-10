6e6ebb248f9957686e7bfdd1524415bd4187c669
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-03-10 21:07:42 +0900

    feat/#85 : 스켈레톤 적용 & api 호출

--- feature/home/src/main/java/com/yapp/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/yapp/feature/home/HomeViewModel.kt
@@ -6,14 +6,18 @@ import androidx.lifecycle.viewModelScope
 import com.yapp.core.ui.component.UserRole
 import com.yapp.core.ui.mvi.MviIntentStore
 import com.yapp.core.ui.mvi.mviIntentStore
+import com.yapp.dataapi.PostsRepository
 import com.yapp.domain.GetUserProfileUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getUserProfileUseCase: GetUserProfileUseCase,
+    private val getNoticeListRepository: PostsRepository,
 ) : ViewModel() {
 
     val store: MviIntentStore<HomeState, HomeIntent, HomeSideEffect> =
@@ -33,29 +37,44 @@ class HomeViewModel @Inject constructor(
         when (intent) {
             HomeIntent.ClickMoreButton -> postSideEffect(HomeSideEffect.NavigateToNotice)
             HomeIntent.ClickSettingButton -> postSideEffect(HomeSideEffect.NavigateToSetting)
-            HomeIntent.LoadMyInfo -> {
-                loadUserInfo(reduce, postSideEffect)
+            HomeIntent.EnterHomeScreen -> {
+                loadUserInfo(state, reduce, postSideEffect)
             }
         }
     }
 
     private fun loadUserInfo(
+        state: HomeState,
         reduce: (HomeState.() -> HomeState) -> Unit,
         postSideEffect: (HomeSideEffect) -> Unit,
     ) = viewModelScope.launch {
+        reduce { copy(isLoading = true, isUserInfoLoading = true, isNoticesLoading = true) }
         getUserProfileUseCase()
-            .onSuccess { info ->
+            .collect { info ->
                 reduce {
                     copy(
+                        isUserInfoLoading = false,
                         name = info.name,
                         role = UserRole.fromRole(info.role),
                         activityUnits = info.activityUnits
                     )
                 }
             }
-            .onFailure {
-                val errorMessage = it.message ?: ""
-                postSideEffect(HomeSideEffect.ShowToast(errorMessage))
+
+        getNoticeListRepository.getNoticeList(noticeType = "전체", lastNoticeId = null, limit = 3)
+            .collect { notices ->
+                reduce { copy(noticeInfo = notices, isNoticesLoading = false) }
             }
+
+        launch {
+            combine(
+                store.uiState.map { it.isUserInfoLoading },
+                store.uiState.map { it.isNoticesLoading },
+            ) { isUserInfoLoading, isNoticesLoading ->
+                (isUserInfoLoading && isNoticesLoading)
+            }.collect { isLoading ->
+                reduce { copy(isLoading = isLoading) }
+            }
+        }
     }
 }
