9459ebaa66053dffefce77e63c57a32359624b9f
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-03-19 18:02:47 +0900

    feat/#94 : 공지사항 상세보기 api & impl 생성

--- core/data/src/main/java/com/yapp/core/data/remote/api/PostsApi.kt
+++ core/data/src/main/java/com/yapp/core/data/remote/api/PostsApi.kt
@@ -1,7 +1,9 @@
 package com.yapp.core.data.remote.api
 
+import com.yapp.core.data.remote.model.response.NoticeData
 import com.yapp.core.data.remote.model.response.NoticeListResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 internal interface PostsApi {
@@ -11,4 +13,7 @@ internal interface PostsApi {
         @Query("limit") limit: Int,
         @Query("noticeType") noticeType: String,
     ) : NoticeListResponse
+
+    @GET("v1/posts/notices/{noticeId}")
+    suspend fun getNoticeItem(@Path("noticeId") noticeId: String): NoticeData
 }
