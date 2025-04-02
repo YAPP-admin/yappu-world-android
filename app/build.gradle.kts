a74b1eb0b2ad5f2cb0c195ba02e2cf22a4501520
jinukeu <koownij@kakao.com>
2025-04-02 23:49:05 +0900

    feat: Increase versionCode to 2
    Increased the `versionCode` in `build.gradle.kts` from 1 to 2. This signifies an update to the application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yapp.app.official"
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         targetSdk = 35
