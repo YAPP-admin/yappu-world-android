e7c3f630e7102a476bdd101d432872f2216b111e
dongchyeon <kimdonghyun90766@gmail.com>
2025-04-10 15:06:06 +0900

    feat/#121 : BottomNavigationBar 구현

--- core/ui/src/main/java/com/yapp/core/ui/component/BottomNavigationBar.kt
+++ core/ui/src/main/java/com/yapp/core/ui/component/BottomNavigationBar.kt
@@ -4,9 +4,12 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
@@ -18,12 +21,57 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.yapp.core.designsystem.theme.YappTheme
+
+@Composable
+fun BottomNavigationBar(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = Color.White,
+    borderWidth: Dp = 1.dp,
+    content: @Composable RowScope.() -> Unit
+) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .navigationBarsPadding()
+            .drawWithContent {
+                drawContent()
+                drawLine(
+                    color = Color(0xFFF5F5F5),
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width, 0f),
+                    strokeWidth = borderWidth.toPx()
+                )
+            },
+        shadowElevation = 10.dp,
+        color = backgroundColor,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+                .selectableGroup()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                content()
+            }
+        }
+    }
+}
 
 @Composable
 fun BottomNavigationBarItem(
@@ -62,8 +110,6 @@ fun BottomNavigationBarItem(
                 contentDescription = stringResource(id = iconTextId),
                 tint = contentColor
             )
-
-            Spacer(modifier = Modifier.height(4.dp))
 
             Text(
                 text = stringResource(id = iconTextId),
