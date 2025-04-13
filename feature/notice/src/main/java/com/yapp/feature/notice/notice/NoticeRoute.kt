d55178ae242de8b25316bd7a76c00ccc039c9663
taeseongyun <yts0646@gmail.com>
2025-04-13 10:15:47 +0900

    refactor#123 : 사용하지 않는 임포트 제거

--- feature/notice/src/main/java/com/yapp/feature/notice/notice/NoticeRoute.kt
+++ feature/notice/src/main/java/com/yapp/feature/notice/notice/NoticeRoute.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
