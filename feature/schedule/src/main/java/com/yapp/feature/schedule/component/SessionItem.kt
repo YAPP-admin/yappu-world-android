f77b9fda3c28e96ad23b99e5584785cc4d84c0a0
dongchyeon <kimdonghyun90766@gmail.com>
2025-04-17 13:26:28 +0900

    ui/#125 : SessionItem 컴포넌트 구현

---
+++ feature/schedule/src/main/java/com/yapp/feature/schedule/component/SessionItem.kt
@@ -0,0 +1,183 @@
+package com.yapp.feature.schedule.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yapp.core.designsystem.theme.YappTheme
+import com.yapp.feature.schedule.R
+
+@Composable
+internal fun SessionItem(
+    id: Long,
+    title: String,
+    status: AttendanceStatus,
+    date: String,
+    dayOfWeek: String,
+    location: String,
+    time: String,
+    onClick: (Long) -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .graphicsLayer {
+                alpha = when (status) {
+                    AttendanceStatus.SCHEDULED -> 1f
+                    AttendanceStatus.ATTENDED, AttendanceStatus.LATE -> 0.5f
+                }
+            }
+            .clickable { onClick(id) }
+            .padding(
+                horizontal = 20.dp,
+                vertical = 16.dp
+            )
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = date,
+                style = YappTheme.typography.body1NormalBold,
+                color = YappTheme.colorScheme.labelNeutral
+            )
+
+            Text(
+                text = "(${dayOfWeek})",
+                style = YappTheme.typography.caption1Medium,
+                color = YappTheme.colorScheme.labelNeutral
+            )
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column(modifier = Modifier.weight(1f)) {
+            Spacer(modifier = Modifier.height(2.dp))
+
+            Text(
+                text = title,
+                style = YappTheme.typography.label1NormalBold,
+                color = YappTheme.colorScheme.labelNormal
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            IconWithText(
+                iconResId = R.drawable.icon_time,
+                text = time,
+                contentDescription = null,
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            IconWithText(
+                iconResId = R.drawable.icon_location,
+                text = location,
+                contentDescription = null,
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+
+        AttendanceStatusChip(status = status)
+    }
+}
+
+@Composable
+internal fun IconWithText(
+    iconResId: Int,
+    text: String,
+    contentDescription: String? = null
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = contentDescription,
+            tint = Color.Unspecified
+        )
+
+        Text(
+            text = text,
+            style = YappTheme.typography.caption1Regular,
+            color = YappTheme.colorScheme.labelAlternative
+        )
+    }
+}
+@Preview(
+    showBackground = true,
+)
+@Composable
+private fun PreviewSessionItem() {
+    YappTheme {
+        Column {
+            SessionItem(
+                id = 1,
+                title = "세션 제목",
+                status = AttendanceStatus.SCHEDULED,
+                date = "12. 6",
+                dayOfWeek = "금",
+                location = "공덕 창업허브",
+                time = "오후 2시 - 오후 6시",
+                onClick = {}
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .padding(horizontal = 20.dp)
+                    .background(YappTheme.colorScheme.lineNormalAlternative)
+            )
+
+            SessionItem(
+                id = 2,
+                title = "세션 제목",
+                status = AttendanceStatus.ATTENDED,
+                date = "12. 5",
+                dayOfWeek = "금",
+                location = "공덕 창업허브",
+                time = "오후 2시 - 오후 6시",
+                onClick = {}
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .padding(horizontal = 20.dp)
+                    .background(YappTheme.colorScheme.lineNormalAlternative)
+            )
+
+            SessionItem(
+                id = 3,
+                title = "세션 제목",
+                status = AttendanceStatus.LATE,
+                date = "12. 4",
+                dayOfWeek = "금",
+                location = "공덕 창업허브",
+                time = "오후 2시 - 오후 6시",
+                onClick = {}
+            )
+        }
+    }
+}
