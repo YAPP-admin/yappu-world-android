73144565fb28799e2f54653a76ca5b902656c3ae
dongchyeon <kimdonghyun90766@gmail.com>
2025-04-17 11:09:43 +0900

    ui/#125 : MonthHeader 컴포넌트 구현

--- feature/schedule/src/main/java/com/yapp/feature/schedule/ScheduleScreen.kt
+++ feature/schedule/src/main/java/com/yapp/feature/schedule/ScheduleScreen.kt
@@ -1,16 +1,24 @@
 package com.yapp.feature.schedule
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,6 +51,21 @@ internal fun ScheduleScreen(
             ),
             onTabSelected = {}
         )
+
+        LazyColumn(
+            modifier = Modifier.padding(horizontal = 20.dp)
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+                MonthHeader(
+                    year = 2024,
+                    month = 12,
+                    onPreviousMonthClick = {},
+                    onNextMonthClick = {}
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
     }
 }
 
@@ -62,6 +85,39 @@ private fun ScheduleHeader(
             text = stringResource(id = R.string.schedule_screen_title),
             style = YappTheme.typography.heading1Bold,
             color = YappTheme.colorScheme.labelNormal,
+        )
+    }
+}
+
+@Composable
+private fun MonthHeader(
+    year: Int,
+    month: Int,
+    onPreviousMonthClick: () -> Unit,
+    onNextMonthClick: () -> Unit,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier.clickable { onPreviousMonthClick() },
+            painter = painterResource(id = R.drawable.icon_arrow_left),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+
+        Text(
+            text = "${year}년 ${month}월",
+            style = YappTheme.typography.headline1Bold,
+            color = YappTheme.colorScheme.labelNormal,
+        )
+
+        Icon(
+            modifier = Modifier.clickable { onNextMonthClick() },
+            painter = painterResource(id = R.drawable.icon_arrow_right),
+            contentDescription = null,
+            tint = Color.Unspecified
         )
     }
 }
