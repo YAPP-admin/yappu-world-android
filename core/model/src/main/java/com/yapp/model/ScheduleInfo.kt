b09a52c3b6d9c4801df74b78c2ee267f382a1f48
dongchyeon <kimdonghyun90766@gmail.com>
2025-04-22 21:42:18 +0900

    feat/#134 : 일정 조회 API data layer 작업 및 domain model 정의

---
+++ core/model/src/main/java/com/yapp/model/ScheduleInfo.kt
@@ -0,0 +1,54 @@
+package com.yapp.model
+
+data class ScheduleList(
+    val dates: List<DateGroupedSchedule>
+)
+
+data class DateGroupedSchedule(
+    val date: String,
+    val schedules: List<ScheduleInfo>
+)
+
+data class ScheduleInfo(
+    val id: String,
+    val name: String,
+    val place: String?,
+    val date: String,
+    val endDate: String?,
+    val time: String?,
+    val scheduleType: ScheduleType,
+    val sessionType: SessionType?,
+    val scheduleProgressPhase: ScheduleProgressPhase,
+    val attendanceStatus: String?,
+)
+
+enum class ScheduleType {
+    SESSION, TASK, ETC;
+
+    companion object {
+        fun fromApiValue(apiValue: String): ScheduleType {
+            return (entries.find { it.name == apiValue }) ?: ETC
+        }
+    }
+}
+
+enum class SessionType {
+    OFFLINE, ONLINE, TEAM;
+
+    companion object {
+        fun fromApiValue(apiValue: String): SessionType {
+            return (entries.find { it.name == apiValue }) ?: OFFLINE
+        }
+    }
+}
+
+enum class ScheduleProgressPhase {
+    DONE, TODAY, ONGOING, PENDING;
+
+    companion object {
+        fun fromApiValue(apiValue: String): ScheduleProgressPhase {
+            return (entries.find { it.name == apiValue }) ?: PENDING
+        }
+    }
+}
+
