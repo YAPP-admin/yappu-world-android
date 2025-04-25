267584ee6ffbaa371b559644b3081b903a7d9ddd
taeseongyun <yts0646@gmail.com>
2025-04-25 14:55:41 +0900

    feature#137 : 이전기수 활동 내역 작업 완료

---
+++ feature/history/src/main/java/com/yapp/feature/history/previous/PreviousHistoryScreen.kt
@@ -0,0 +1,93 @@
+package com.yapp.feature.history.previous
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.yapp.core.designsystem.component.header.YappHeaderActionbar
+import com.yapp.core.designsystem.theme.YappTheme
+import com.yapp.core.ui.component.YappBackground
+import com.yapp.core.ui.extension.collectWithLifecycle
+import com.yapp.feature.history.R
+import com.yapp.feature.history.previous.PreviousHistoryIntent as Intent
+import com.yapp.feature.history.previous.PreviousHistorySideEffect as SideEffect
+import com.yapp.feature.history.previous.PreviousHistoryState.History
+
+@Composable
+internal fun PreviousHistoryRoute(
+    viewModel: PreviousHistoryViewModel = hiltViewModel(),
+    onClickBackButton: () -> Unit
+) {
+    val uiState by viewModel.store.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.store.onIntent(Intent.OnEntryScreen)
+    }
+
+    viewModel.store.sideEffects.collectWithLifecycle { effect ->
+        when (effect) {
+            SideEffect.Finish -> {
+                onClickBackButton()
+            }
+        }
+    }
+
+    BackHandler { viewModel.store.onIntent(Intent.OnClickBackButton) }
+
+    PreviousHistoryScreen(
+        items = uiState.items,
+        onClickBackButton = onClickBackButton
+    )
+}
+
+
+@Composable
+private fun PreviousHistoryScreen(
+    items: List<History>,
+    onClickBackButton: () -> Unit
+) {
+    YappBackground {
+        Column {
+            YappHeaderActionbar(
+                title = stringResource(R.string.previous_title),
+                leftIcon = com.yapp.core.designsystem.R.drawable.icon_chevron_left,
+                onClickLeftIcon = onClickBackButton
+            )
+
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                contentPadding = PaddingValues(20.dp)
+            ) {
+                items(items) { item ->
+                    HistoryItems(
+                        modifier = Modifier.background(
+                            color = YappTheme.colorScheme.orange99,
+                            shape = RoundedCornerShape(12.dp)
+                        ).padding(16.dp),
+                        position = item.position,
+                        generation = item.generation,
+                        slot = {
+                            if (item.activityEndDate.orEmpty().isNotEmpty() && item.activityStartDate.orEmpty().isNotEmpty()) {
+                                Text("${item.activityStartDate} - ${item.activityEndDate}")
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
