a9c5d5f37487b922c3a09f988e37f09431f1e496
taeseongyun <yts0646@gmail.com>
2025-04-28 22:52:16 +0900

    chore/#137 : 리베이스 후 적용 되어있지 않은 클래스 수정

--- feature/history/src/main/java/com/yapp/feature/history/attend/AttendHistoryUseCase.kt
+++ feature/history/src/main/java/com/yapp/feature/history/attend/AttendHistoryUseCase.kt
@@ -1,17 +1,17 @@
 package com.yapp.feature.history.attend
 
 import com.yapp.dataapi.AttendanceRepository
-import com.yapp.dataapi.SessionRepository
+import com.yapp.dataapi.ScheduleRepository
 import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 internal class AttendHistoryUseCase @Inject constructor(
     private val attendanceRepository: AttendanceRepository,
-    private val sessionRepository: SessionRepository
+    private val scheduleRepository: ScheduleRepository
 ){
     operator fun invoke() = combine(
         attendanceRepository.getAttendanceStatistics(),
-        sessionRepository.getSessions()
+        scheduleRepository.getSessions()
     ) { attendStatistics, sessions ->
         attendStatistics to sessions
     }
