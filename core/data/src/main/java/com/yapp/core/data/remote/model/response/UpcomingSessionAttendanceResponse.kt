bd140f52001964be33b966d1c6f1b58fb68b70ab
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-02 18:09:04 +0900

    refactor/#140: 변수명과 JSON 필드 명이 같기 때문에 SerialName 제거

--- core/data/src/main/java/com/yapp/core/data/remote/model/response/UpcomingSessionAttendanceResponse.kt
+++ core/data/src/main/java/com/yapp/core/data/remote/model/response/UpcomingSessionAttendanceResponse.kt
@@ -2,35 +2,22 @@ package com.yapp.core.data.remote.model.response
 
 import com.yapp.core.data.remote.model.response.ScheduleResponse.Companion.toAttendanceStatus
 import com.yapp.model.UpcomingSessionInfo
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlin.math.max
 
 @Serializable
 data class UpcomingSessionAttendanceResponse(
-    @SerialName("sessionId")
     val sessionId: String,
-    @SerialName("name")
     val name: String,
-    @SerialName("startDate")
     val startDate: String,
-    @SerialName("startDayOfWeek")
     val startDayOfWeek: String,
-    @SerialName("endDate")
     val endDate: String,
-    @SerialName("endDayOfWeek")
     val endDayOfWeek: String,
-    @SerialName("startTime")
     val startTime: String?,
-    @SerialName("endTime")
     val endTime: String?,
-    @SerialName("place")
     val place: String?,
-    @SerialName("relativeDays")
     val relativeDays: Int,
-    @SerialName("canCheckIn")
     val canCheckIn: Boolean,
-    @SerialName("status")
     val status: String?
 ) {
     fun toUpcomingSessionInfoModel() = UpcomingSessionInfo(
