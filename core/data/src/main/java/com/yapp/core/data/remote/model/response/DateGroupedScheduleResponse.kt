cf8b8fffd542d63d6895108473d5eedb0535fb03
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-02 18:09:04 +0900

    refactor/#140: 변수명과 JSON 필드 명이 같기 때문에 SerialName 제거

--- core/data/src/main/java/com/yapp/core/data/remote/model/response/DateGroupedScheduleResponse.kt
+++ core/data/src/main/java/com/yapp/core/data/remote/model/response/DateGroupedScheduleResponse.kt
@@ -7,12 +7,10 @@ import com.yapp.model.ScheduleList
 import com.yapp.model.ScheduleProgressPhase
 import com.yapp.model.ScheduleType
 import com.yapp.model.SessionType
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DateGroupedScheduleResponse(
-    @SerialName("dates")
     val dates: List<ScheduleListResponse>
 ) {
     fun toScheduleListModel() = ScheduleList(
@@ -24,16 +22,9 @@ data class DateGroupedScheduleResponse(
 
 @Serializable
 data class ScheduleListResponse(
-    @SerialName("date")
     val date: String,
-
-    @SerialName("isToday")
     val isToday: Boolean,
-
-    @SerialName("dayOfTheWeek")
     val dayOfTheWeek: String,
-
-    @SerialName("schedules")
     val schedules: List<ScheduleResponse>,
 ) {
     fun toScheduleListModel() = DateGroupedSchedule(
@@ -46,37 +37,16 @@ data class ScheduleListResponse(
 
 @Serializable
 data class ScheduleResponse(
-    @SerialName("id")
     val id: String,
-
-    @SerialName("name")
     val name: String,
-
-    @SerialName("place")
     val place: String?,
-
-    @SerialName("date")
     val date: String,
-
-    @SerialName("endDate")
     val endDate: String?,
-
-    @SerialName("time")
     val time: String?,
-
-    @SerialName("endTime")
     val endTime: String?,
-
-    @SerialName("scheduleType")
     val scheduleType: String,
-
-    @SerialName("sessionType")
     val sessionType: String?,
-
-    @SerialName("scheduleProgressPhase")
     val scheduleProgressPhase: String,
-
-    @SerialName("attendanceStatus")
     val attendanceStatus: String?,
 ) {
     fun toScheduleModel() = ScheduleInfo(
