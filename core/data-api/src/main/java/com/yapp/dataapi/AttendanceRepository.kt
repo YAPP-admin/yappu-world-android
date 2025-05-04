050b307e4319c2c56b6bbe5c93e271cf193eb913
taeseongyun <yts0646@gmail.com>
2025-05-04 09:32:13 +0900

    feature/#146 : 출석 처리 api 작업 완료

--- core/data-api/src/main/java/com/yapp/dataapi/AttendanceRepository.kt
+++ core/data-api/src/main/java/com/yapp/dataapi/AttendanceRepository.kt
@@ -1,8 +1,10 @@
 package com.yapp.dataapi
 
 import com.yapp.model.AttendStatistics
+import com.yapp.model.AttendanceInfo
 import kotlinx.coroutines.flow.Flow
 
 interface AttendanceRepository {
     fun getAttendanceStatistics(): Flow<AttendStatistics>
+    suspend fun postAttendance(attendance: AttendanceInfo)
 }
