a7ba596e39fd94da0e5936bd14afd34ce766f63f
jinukeu <koownij@kakao.com>
2025-05-04 23:57:35 +0900

    refactor/#144: http uri https 로 변경

--- core/ui/src/main/java/com/yapp/core/ui/extension/UriHandler.kt
+++ core/ui/src/main/java/com/yapp/core/ui/extension/UriHandler.kt
@@ -5,10 +5,10 @@ import com.yapp.core.common.android.record
 import timber.log.Timber
 
 fun UriHandler.safeOpenUri(rawUrl: String) {
-    val url = if (!rawUrl.startsWith("http://") && !rawUrl.startsWith("https://")) {
-        "https://$rawUrl"
-    } else {
-        rawUrl
+    val url = when {
+        rawUrl.startsWith("https://") -> rawUrl
+        rawUrl.startsWith("http://") -> "https://" + rawUrl.removePrefix("http://")
+        else -> "https://$rawUrl"
     }
 
     try {
