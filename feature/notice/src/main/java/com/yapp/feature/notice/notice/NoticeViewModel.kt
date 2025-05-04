c677e19f8c9f8c9e050d05842bec62eae3a4549d
jinukeu <koownij@kakao.com>
2025-05-04 23:09:27 +0900

    refactor/#146: 공지사항 관련 API 함수 suspend 제거
    데이터 레이어 함수 suspend 키워드 제거

--- feature/notice/src/main/java/com/yapp/feature/notice/notice/NoticeViewModel.kt
+++ feature/notice/src/main/java/com/yapp/feature/notice/notice/NoticeViewModel.kt
@@ -2,20 +2,23 @@ package com.yapp.feature.notice.notice
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yapp.core.common.android.record
 import com.yapp.core.ui.mvi.MviIntentStore
 import com.yapp.core.ui.mvi.mviIntentStore
 import com.yapp.dataapi.PostsRepository
 import com.yapp.model.NoticeType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NoticeViewModel @Inject constructor(
     private val getNoticeListRepository: PostsRepository,
-
-    ) : ViewModel() {
+) : ViewModel() {
     val store: MviIntentStore<NoticeState, NoticeIntent, NoticeSideEffect> =
         mviIntentStore(
             initialState = NoticeState(),
@@ -46,28 +49,41 @@ class NoticeViewModel @Inject constructor(
     private fun loadNotices(
         state: NoticeState,
         reduce: (NoticeState.() -> NoticeState) -> Unit,
-    ) = viewModelScope.launch {
+    ) {
 
         if (state.isNoticeEmpty) {
             reduce { copy(isNoticesLoading = true) }
         }
 
         getNoticeListRepository
-            .getNoticeList(state.notices.lastNoticeId.ifEmpty { null }, 30, state.noticeType.apiValue)
-            .collectLatest { noticeList ->
-                reduce { copy(notices = noticeList.copy(notices = noticeList.notices + state.notices.notices), isNoticesLoading = false) }
+            .getNoticeList(
+                state.notices.lastNoticeId.ifEmpty { null },
+                30,
+                state.noticeType.apiValue
+            )
+            .onEach { noticeList ->
+                reduce {
+                    copy(
+                        notices = noticeList.copy(notices = noticeList.notices + state.notices.notices),
+                        isNoticesLoading = false
+                    )
+                }
             }
+            .catch { it.record() }
+            .launchIn(viewModelScope)
     }
 
     private fun loadNoticeTypeList(
         reduce: (NoticeState.() -> NoticeState) -> Unit,
         noticeType: NoticeType,
-    ) = viewModelScope.launch{
+    ) {
         reduce { copy(noticeType = noticeType, isNoticesLoading = true) }
         getNoticeListRepository
             .getNoticeList(null, 30, noticeType.apiValue)
-            .collectLatest { noticeList ->
+            .onEach { noticeList ->
                 reduce { copy(notices = noticeList, isNoticesLoading = false) }
             }
+            .catch { it.record() }
+            .launchIn(viewModelScope)
     }
 }
