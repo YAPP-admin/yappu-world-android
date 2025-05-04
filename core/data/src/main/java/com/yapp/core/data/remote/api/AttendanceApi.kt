d811466de7eac403399ab9b3acfb527d58e1ddfc
taeseongyun <yts0646@gmail.com>
2025-05-04 09:32:13 +0900

    feature/#146 : 출석 처리 api 작업 완료

--- core/data/src/main/java/com/yapp/core/data/remote/api/AttendanceApi.kt
+++ core/data/src/main/java/com/yapp/core/data/remote/api/AttendanceApi.kt
@@ -1,12 +1,14 @@
 package com.yapp.core.data.remote.api
 
+import com.yapp.core.data.remote.model.request.Attendance
 import com.yapp.core.data.remote.model.response.AttendStatisticsResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AttendanceApi {
     @POST("v1/attendances")
-    suspend fun postAttendance()
+    suspend fun postAttendance(@Body request: Attendance)
 
     @GET("v1/attendances/statistics")
     suspend fun getAttendanceStatistics(): AttendStatisticsResponse
