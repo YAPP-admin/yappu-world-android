7075c0e7bc5228f4e2c7eba7a5c60afe5a4dbe83
jinukeu <koownij@kakao.com>
2025-05-05 23:56:26 +0900

    chore/#164: Scaffold에 contentWindowInsets = WindowInsets(0.dp) 추가

--- app/src/main/java/com/yapp/app/official/ui/YappApp.kt
+++ app/src/main/java/com/yapp/app/official/ui/YappApp.kt
@@ -3,10 +3,12 @@ package com.yapp.app.official.ui
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -36,6 +38,7 @@ fun YappApp(navigator: NavigatorState) {
                 )
             }
         },
+        contentWindowInsets = WindowInsets(0.dp),
     ) { padding ->
         YappNavHost(navigator, modifier = Modifier.padding(padding))
     }
