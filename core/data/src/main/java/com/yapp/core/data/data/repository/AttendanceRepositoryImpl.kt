8d2e203a20e4920c8d517e541ecff89e36c3a308
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-05 22:37:48 +0900

    refactor/#155: 출석 통계 API Flow 반환 메소드를 suspend fun으로 변경

--- core/data/src/main/java/com/yapp/core/data/data/repository/AttendanceRepositoryImpl.kt
+++ core/data/src/main/java/com/yapp/core/data/data/repository/AttendanceRepositoryImpl.kt
@@ -7,16 +7,13 @@ import com.yapp.dataapi.AttendanceRepository
 import com.yapp.model.AttendStatistics
 import com.yapp.model.AttendanceHistoryList
 import com.yapp.model.AttendanceInfo
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 internal class AttendanceRepositoryImpl @Inject constructor(
     private val attendApi: AttendanceApi
 ): AttendanceRepository{
-    override fun getAttendanceStatistics(): Flow<AttendStatistics> = flow {
-        emit(attendApi.getAttendanceStatistics().toModel())
-    }
+    override suspend fun getAttendanceStatistics(): AttendStatistics =
+        attendApi.getAttendanceStatistics().toModel()
 
     override suspend fun getAttendanceHistory(): AttendanceHistoryList =
         attendApi.getAttendanceHistory().toAttendanceHistoryListModel()
