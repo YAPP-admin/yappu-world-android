9c72641f631e733082f98cd0503831c7bd63a8b3
jinukeu <koownij@kakao.com>
2025-05-05 23:56:13 +0900

    feat/#164: Introduce `YappBackground` with Window Insets Handling
    Introduces the `YappBackground` composable function to provide a consistent background for screens.
    This component wraps its content in a `Surface` with the specified background color.
    It also includes handling for window insets (`WindowInsets.systemBars` by default) using `onConsumedWindowInsetsChanged` and `exclude` to apply appropriate padding and avoid overlapping with system UI elements.

--- core/ui/src/main/java/com/yapp/core/ui/component/YappBackground.kt
+++ core/ui/src/main/java/com/yapp/core/ui/component/YappBackground.kt
@@ -1,21 +1,44 @@
 package com.yapp.core.ui.component
 
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.MutableWindowInsets
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.onConsumedWindowInsetsChanged
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.yapp.core.designsystem.theme.YappTheme
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun YappBackground(
     modifier: Modifier = Modifier,
     color: Color = YappTheme.colorScheme.backgroundElevatedNormal,
+    contentWindowInsets: WindowInsets = WindowInsets.systemBars,
     content: @Composable () -> Unit,
 ) {
+    val safeInsets = remember(contentWindowInsets) {
+        MutableWindowInsets(
+            contentWindowInsets
+        )
+    }
+
     Surface(
         color = color,
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .onConsumedWindowInsetsChanged { consumedWindowInsets ->
+                // Exclude currently consumed window insets from user provided contentWindowInsets
+                safeInsets.insets = contentWindowInsets.exclude(consumedWindowInsets)
+            }
+            .padding(safeInsets.insets.asPaddingValues()),
     ) {
         content()
     }
