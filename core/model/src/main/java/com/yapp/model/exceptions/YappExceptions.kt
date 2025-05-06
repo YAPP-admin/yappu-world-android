e546df6111c3182d2a5e1e5b3e1c3a7e6d8c7501
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-06 15:01:06 +0900

    feat/#167: ATD_1001 에러 정의

--- core/model/src/main/java/com/yapp/model/exceptions/YappExceptions.kt
+++ core/model/src/main/java/com/yapp/model/exceptions/YappExceptions.kt
@@ -19,20 +19,24 @@ enum class YappServerError(val exception: YappException) {
 
     //토큰 만료
     TKN_0001(InvalidTokenException()),
-    TKN_0002(InvalidTokenException())
+    TKN_0002(InvalidTokenException()),
+
+    //출석 체크
+    ATD_1001(CodeNotCorrectException()),
 }
 
 class InternalServerException : YappException()
 class InvalidRequestArgument : YappException()
-class SignUpCodeException() : YappException("잘못된 가입코드입니다.")
-class AlreadyRegisteredException() : YappException("이미 가입된 이메일입니다.")
-class UnprocessedSignUpException() : YappException("처리되지 않은 가입 신청이 존재하여, 추가 가입 신청이 불가합니다.")
+class SignUpCodeException : YappException("잘못된 가입코드입니다.")
+class AlreadyRegisteredException : YappException("이미 가입된 이메일입니다.")
+class UnprocessedSignUpException : YappException("처리되지 않은 가입 신청이 존재하여, 추가 가입 신청이 불가합니다.")
 class UserNotFoundForEmailException : YappException("계정 정보를 찾을 수 없습니다.")
 class SignUpPendingException : YappException("회원가입 처리가 진행 중입니다.")
 class RecentSignUpRejectedException : YappException("최근의 회원가입 신청은 거절되었습니다.")
 class LoginBlockedException : YappException("로그인이 불가능한 회원 상태입니다.")
 class LoginException : YappException("로그인에 실패했습니다. 계정 정보를 다시 확인하세요.")
 class InvalidTokenException : YappException("비정상 토큰입니다")
+class CodeNotCorrectException : YappException("출석 코드가 일치하지 않습니다.")
 
 open class YappException(message: String = "") : Exception(message) {
     private var _message: String = message
@@ -44,5 +48,3 @@ open class YappException(message: String = "") : Exception(message) {
         _message = newMessage
     }
 }
-
-
