84c803c2844819a4d05d68790e727c5c4dc1425d
jinukeu <koownij@kakao.com>
2025-05-17 17:27:07 +0900

    fix/#195: 앱 기본 폰트 크기 1.0f로 고정

--- app/src/main/java/com/yapp/app/official/YappOfficialApplication.kt
+++ app/src/main/java/com/yapp/app/official/YappOfficialApplication.kt
@@ -1,8 +1,6 @@
 package com.yapp.app.official
 
 import android.app.Application
-import android.content.Context
-import android.content.res.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -15,13 +13,5 @@ class YappOfficialApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-    }
-
-    // 앱 기본 폰트 크기를 1.0f로 고정합니다.
-    override fun attachBaseContext(base: Context?) {
-        val configuration = Configuration(base?.resources?.configuration)
-        configuration.fontScale = 1.0f
-        val context = base?.createConfigurationContext(configuration)
-        super.attachBaseContext(context)
     }
 }
