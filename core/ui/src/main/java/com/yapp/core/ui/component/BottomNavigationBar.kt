dc989262ff5ee401a25c00c2e26ac4d9cf3ee898
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-07-15 15:24:55 +0900

    refactor/#207 : CompositionLocalProvider 사용하여 높이 계산

--- core/ui/src/main/java/com/yapp/core/ui/component/BottomNavigationBar.kt
+++ core/ui/src/main/java/com/yapp/core/ui/component/BottomNavigationBar.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
@@ -25,6 +26,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.yapp.core.designsystem.theme.YappTheme
+
+val LocalBottomBarHeight = compositionLocalOf { 56.dp }
 
 @Composable
 fun BottomNavigationBar(
