d2faf8cfea786d43501c09d849693b62a022ab26
taeseongyun <yts0646@gmail.com>
2025-10-21 22:14:46 +0900

    chore/#225 : 세션 리스폰스 Enum 값 매핑 잘못되어있던 현상 수정

--- core/data/src/main/java/com/yapp/core/data/remote/model/response/SessionResponse.kt
+++ core/data/src/main/java/com/yapp/core/data/remote/model/response/SessionResponse.kt
@@ -80,5 +80,5 @@ fun SessionResponse.Session.toHomeSessionModel(): HomeSession {
 }
 
 fun String.toSessionProgressPhase() =
-    SessionProgressPhase.entries.firstOrNull { it.name == this }
+    SessionProgressPhase.entries.firstOrNull { it.title == this }
         ?: SessionProgressPhase.PENDING
